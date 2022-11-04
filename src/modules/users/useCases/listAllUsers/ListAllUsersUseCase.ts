8ee632f9b4a310aab10f9106699b7c2f6e37f10c
Gabriel <gabriel.fernandesb@gmail.com>
2022-11-04 16:51:37 -0300

    feat: exercise completed

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,12 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) throw new Error("User doesn't exist!");
+    if (!user.admin) throw new Error("You must be an admin!");
+
+    return this.usersRepository.list();
   }
 }
 
--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,13 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+
+      return response.send(this.showUserProfileUseCase.execute({ user_id }));
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
